620d89c368f3becf2441b596ac7716f67826c06d
diego.salazar01 <diego.salazar01@globant.com>
2025-09-08 14:52:15 -0500

    test: add available pets test with status validation

---
+++ src/test/java/com/automationProject/model/pet/Pet.java
@@ -0,0 +1,23 @@
+package com.automationProject.model.pet;
+
+import com.automationProject.model.category.Category;
+import com.automationProject.model.tag.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+    private Long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+    private String status;
+}
